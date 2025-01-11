621f30b7c18c35d7d47a8c2753047e8c2993185d
승환 지 <horong0602.dev@gmail.com>
2025-01-12 04:25:57 +0900

    플레이어, 투사체 오브젝트

---
+++ 18강 - Object(2)/CPlayer.cpp
@@ -0,0 +1,54 @@
+#include "pch.h"
+#include "CPlayer.h"
+#include "CScene.h"
+#include "CSceneMgr.h"
+#include "CKeyMgr.h"
+#include "CTimeMgr.h"
+#include "CMissile.h"
+
+void CPlayer::update()
+{
+	Vec2 vPos = GetPos(); // 부모 클래스를 통해 좌표 받아오기.
+
+	if (KEY_HOLD(KEY::UP))
+	{
+		vPos.y -= 200.f * fDT;
+	}
+
+	if (KEY_HOLD(KEY::DOWN))
+	{
+		vPos.y += 200.f * fDT;
+	}
+
+	if (KEY_HOLD(KEY::LEFT))
+	{
+		vPos.x -= 200.f * fDT;
+	}
+
+	if (KEY_HOLD(KEY::RIGHT))
+	{
+		vPos.x += 200.f * fDT;
+	}
+
+	if (KEY_TAP(KEY::A))
+	{
+		CreateMissile();
+	}
+
+	SetPos(vPos); // 부모 클래스를 통해 좌표 갱신.
+}
+
+void CPlayer::CreateMissile()
+{
+	Vec2 vMissilePos = GetPos();
+	vMissilePos.y -= GetScale().y / 2.f;
+
+	// Missile Object
+	CMissile* pMissile = new CMissile;
+	pMissile->SetPos(vMissilePos);
+	pMissile->SetScale(Vec2(25.f, 25.f));
+	pMissile->SetDir(true);
+
+	CScene* pCurScene = CSceneMgr::GetInst()->GetCurScene();
+	pCurScene->AddObject(pMissile, GROUP_TYPE::DEFAULT);
+}
---
+++ 18강 - Object(2)/define.h
@@ -0,0 +1,53 @@
+#pragma once
+
+// 싱글톤 매크로 함수
+// #define SINGLE(type) public: static type* GetInst() {static type mgr; return &mgr;}
+// mgr: manager 약자
+
+// \ 를 사용하면 다음 줄도 전처리기 치환 구문에 포함되어 매크로 정의를 이어갈 수 있다.
+#define SINGLE(type) public:\
+						 static type* GetInst()\
+						 {\
+							 static type mgr;\
+							 \
+							 return &mgr;\
+						 }\
+					 private:\
+						type();\
+						~type();
+
+// 델타 타임(프레임 사이의 흐른 시간) 반환 매크로 함수.
+#define fDT CTimeMgr::GetInst()->GetfDT()
+#define DT CTimeMgr::GetInst()->GetDT()
+
+// 키 입력 확인 매크로.
+#define KEY_CHECK(key, state) CKeyMgr::GetInst()->GetKeyState(key) == state
+#define KEY_HOLD(key) KEY_CHECK(key, KEY_STATE::HOLD)
+#define KEY_TAP(key) KEY_CHECK(key, KEY_STATE::TAP)
+#define KEY_AWAY(key) KEY_CHECK(key, KEY_STATE::AWAY)
+#define KEY_NONE(key) KEY_CHECK(key, KEY_STATE::NONE)
+
+// 오브젝트 종류 매크로.
+enum class GROUP_TYPE
+{
+	DEFAULT,
+
+	PLAYER,
+
+	MISSILE,
+
+	MONSTER,
+
+	END = 32,
+};
+
+// 씬 종류 매크로.
+enum class SCENE_TYPE
+{
+	TOOL,
+	START,
+	STAGE_01,
+	STAGE_02,
+
+	END,
+};
