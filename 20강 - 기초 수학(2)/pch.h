0d6f22e4d65c79a0fab1a818d0504f964b576b10
승환 지 <horong0602.dev@gmail.com>
2025-01-14 05:53:24 +0900

    벡터 정규화, 미사일 발사 각도

---
+++ 20강 - 기초 수학(2)/pch.h
@@ -0,0 +1,18 @@
+#pragma once
+
+// 미리 컴파일된 헤더
+#include <Windows.h> // 윈도우 기능 헤더.
+#include <iostream>
+
+#include <vector>
+using std::vector;
+
+#include <string>
+using std::string;
+using std::wstring;
+
+#include <math.h>
+#include <assert.h>
+
+#include "define.h"
+#include "struct.h"
