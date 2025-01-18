38617efb3c229b2d537b9da1f48cb29d80beac11
승환 지 <horong0602.dev@gmail.com>
2025-01-19 03:37:25 +0900

    충돌 영역 표시

--- 26강 - Collider(2)/define.h
+++ 26강 - Collider(2)/define.h
@@ -1,10 +1,6 @@
 #pragma once
 
-// 싱글톤 매크로 함수
-// #define SINGLE(type) public: static type* GetInst() {static type mgr; return &mgr;}
-// mgr: manager 약자
-
-// \ 를 사용하면 다음 줄도 전처리기 치환 구문에 포함되어 매크로 정의를 이어갈 수 있다.
+// 싱글톤 매크로 함수.
 #define SINGLE(type) public:\
 						 static type* GetInst()\
 						 {\
@@ -20,16 +16,16 @@
 #define fDT CTimeMgr::GetInst()->GetfDT()
 #define DT CTimeMgr::GetInst()->GetDT()
 
-// 키 입력 확인 매크로.
+// 키 입력 확인 매크로 함수.
 #define KEY_CHECK(key, state) CKeyMgr::GetInst()->GetKeyState(key) == state
 #define KEY_HOLD(key) KEY_CHECK(key, KEY_STATE::HOLD)
 #define KEY_TAP(key) KEY_CHECK(key, KEY_STATE::TAP)
 #define KEY_AWAY(key) KEY_CHECK(key, KEY_STATE::AWAY)
 #define KEY_NONE(key) KEY_CHECK(key, KEY_STATE::NONE)
 
-#define PI 3.1415926535f // 파이 정의.
+#define PI 3.1415926535f // 파이(π) 정의.
 
-// 오브젝트 종류 매크로.
+// 오브젝트 종류.
 enum class GROUP_TYPE
 {
 	DEFAULT,
@@ -43,7 +39,7 @@ enum class GROUP_TYPE
 	END = 32,
 };
 
-// 씬 종류 매크로.
+// 씬 종류.
 enum class SCENE_TYPE
 {
 	TOOL,
@@ -54,7 +50,7 @@ enum class SCENE_TYPE
 	END,
 };
 
-// 오브젝트 충돌 영역 표시에 DC가 사용할 펜 및 브러쉬 종류 매크로.
+// 오브젝트 충돌 영역 표시에 DC가 사용할 펜 및 브러쉬 종류.
 enum class BRUSH_TYPE
 {
 	HOLLOW,
