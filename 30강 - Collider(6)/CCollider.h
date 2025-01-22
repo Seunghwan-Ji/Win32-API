59488dc9cce80ad3fcd12a079e7c2e4282bc3123
승환 지 <horong0602.dev@gmail.com>
2025-01-23 04:55:44 +0900

    시각적으로 충돌 확인

---
+++ 30강 - Collider(6)/CCollider.h
@@ -0,0 +1,49 @@
+#pragma once
+
+class CObject;
+
+class CCollider
+{
+private:
+	static UINT g_iNextID; // 객체에 포함되지 않는 정적 멤버 변수.
+
+	CObject* m_pOwner;	   // collider 를 소유하고 있는 오브젝트.
+	Vec2	 m_vOffsetPos; // 오브젝트로부터 상대적인 위치.
+	Vec2	 m_vFinalPos;  // finalupdate 에서 매 프레임마다 계산.
+	Vec2	 m_vScale;     // 충돌 영역의 사이즈.
+
+	UINT	 m_iID;		   // 충돌 영역의 고유한 ID 값.
+	int		 m_iCol;	   // 시각적 충돌 확인용 멤버 변수.
+
+public:
+	void SetOffsetPos(Vec2 _vPos) { m_vOffsetPos = _vPos; }
+	void SetScale(Vec2 _vScale) { m_vScale = _vScale; }
+
+	Vec2 GetOffsetPos() { return m_vOffsetPos; }
+	Vec2 GetScale() { return m_vScale; }
+
+	Vec2 GetFinalPos() { return m_vFinalPos; }
+
+	UINT GetID() { return m_iID; }
+
+public:
+	void finalupdate(); // 충돌 영역을 오브젝트의 위치로 이동.
+	void render(HDC _dc);
+
+public:
+	// 충돌 시점 함수.
+	void OnCollision(CCollider* _pOther); // 충돌중인 경우 호출되는 함수.
+	void OnCollisionEnter(CCollider* _pOther); // 충돌 진입 시 호출되는 함수.
+	void OnCollisionExit(CCollider* _pOther); // 충돌 해제 시 호출되는 함수.
+
+	CCollider& operator = (CCollider& _origin) = delete;
+	// CCollider 객체끼리는 값을 복사받지 못하게 함.
+	// 대입 연산자 함수 삭제.
+
+public:
+	CCollider();
+	CCollider(const CCollider& _origin); // 복사 생성자.(충돌 영역 복사)
+	~CCollider();
+
+	friend class CObject;
+};
---
+++ 30강 - Collider(6)/CCollisionMgr.cpp
@@ -0,0 +1,145 @@
+#include "pch.h"
+#include "CCollisionMgr.h"
+#include "CSceneMgr.h"
+#include "CScene.h"
+#include "CObject.h"
+#include "CCollider.h"
+
+CCollisionMgr::CCollisionMgr()
+	: m_arrCheck{}
+{
+}
+
+CCollisionMgr::~CCollisionMgr()
+{
+}
+
+void CCollisionMgr::update()
+{
+	// 행 순회.
+	for (UINT iRow = 0; iRow < (UINT)GROUP_TYPE::END; ++iRow)
+	{
+		// 열 순회.
+		for (UINT iCol = iRow; iCol < (UINT)GROUP_TYPE::END; ++iCol)
+			// 중복 검사 방지를 위해 열 시작 번호는 행 번호와 동일.
+		{
+			if (m_arrCheck[iRow] & (1 << iCol)) // iCol번째 비트가 1인지 확인.
+			{
+				CollisionGroupUpdate((GROUP_TYPE)iRow, (GROUP_TYPE)iCol);
+			}
+		}
+	}
+}
+
+void CCollisionMgr::CollisionGroupUpdate(GROUP_TYPE _eLeft, GROUP_TYPE _eRight)
+{
+	CScene* pCurScene = CSceneMgr::GetInst()->GetCurScene();
+
+	const vector<CObject*>& vecLeft = pCurScene->GetGroupObject(_eLeft);
+	const vector<CObject*>& vecRight = pCurScene->GetGroupObject(_eRight);
+
+	map<ULONGLONG, bool>::iterator iter;
+
+	for (size_t i = 0; i < vecLeft.size(); ++i)
+	{
+		// 왼쪽 그룹에 속해있는 오브젝트가 충돌 기능이 있는지 확인.
+		if (nullptr == vecLeft[i]->GetCollider())
+		{
+			continue;
+		}
+
+		for (size_t j = 0; j < vecRight.size(); ++j)
+		{
+			// 오른쪽 그룹에 속해있는 오브젝트가 충돌 기능이 있는지 확인.
+			// + 자기 자신 오브젝트와의 충돌 방지 조건.
+			if (nullptr == vecRight[j]->GetCollider()
+				|| vecLeft[i] == vecRight[j])
+			{
+				continue;
+			}
+
+			CCollider* pLeftCol = vecLeft[i]->GetCollider();
+			CCollider* pRightCol = vecRight[j]->GetCollider();
+
+			// 두 충돌체 조합 ID 생성.
+			COLLIDER_ID ID;
+			ID.Left_id = pLeftCol->GetID();
+			ID.Right_id = pRightCol->GetID();
+
+			iter = m_mapColInfo.find(ID.ID); // ID 탐색.
+
+			// 충돌 정보가 미등록 상태인 경우.
+			if (m_mapColInfo.end() == iter) // ID 를 찾지 못했을 경우.
+			{
+				m_mapColInfo.insert(make_pair(ID.ID, false)); // 등록.
+				iter = m_mapColInfo.find(ID.ID); // 재탐색.
+			}
+
+			if (IsCollision(pLeftCol, pRightCol))
+			{ // 현재 충돌중.
+				if (iter->second)
+				{ // 이전에도 충돌.
+					pLeftCol->OnCollision(pRightCol);
+					pRightCol->OnCollision(pLeftCol);
+				}
+				else
+				{ // 이전에는 충돌하지 않음.
+					pLeftCol->OnCollisionEnter(pRightCol);
+					pRightCol->OnCollisionEnter(pLeftCol);
+					iter->second = true;
+				}
+			}
+			else
+			{ // 현재 충돌중 아님.
+				if (iter->second)
+				{ // 이전에는 충돌중이었음.
+					pLeftCol->OnCollisionExit(pRightCol);
+					pRightCol->OnCollisionExit(pLeftCol);
+					iter->second = false;
+				}
+			}
+		}
+	}
+}
+
+bool CCollisionMgr::IsCollision(CCollider* _pLeftCol, CCollider* _pRightCol)
+{
+	Vec2 vLeftPos = _pLeftCol->GetFinalPos();
+	Vec2 vLeftScale = _pLeftCol->GetScale();
+
+	Vec2 vRightPos = _pRightCol->GetFinalPos();
+	Vec2 vRightScale = _pRightCol->GetScale();
+
+	// 충돌 영역이 서로 겹치는지 검사.
+	if (abs(vRightPos.x - vLeftPos.x) < (vLeftScale.x + vRightScale.x) / 2.f
+		&& abs(vRightPos.y - vLeftPos.y) < (vLeftScale.y + vRightScale.y) / 2.f)
+	{
+		return true;
+	}
+
+	return false;
+}
+
+void CCollisionMgr::CheckGroup(GROUP_TYPE _eLeft, GROUP_TYPE _eRight)
+{
+	// 더 작은 값의 그룹 타입을 행으로,
+	// 큰 값을 열(비트) 로 사용.
+
+	UINT iRow = (UINT)_eLeft;
+	UINT iCol = (UINT)_eRight;
+
+	if (iCol < iRow)
+	{
+		iRow = (UINT)_eRight;
+		iCol = (UINT)_eLeft;
+	}
+
+	if (m_arrCheck[iRow] & (1 << iCol)) // iCol번째 비트가 1인지 확인.
+	{
+		m_arrCheck[iRow] &= ~(1 << iCol); // iCol번째 비트를 0으로 설정.
+	}
+	else
+	{
+		m_arrCheck[iRow] |= (1 << iCol); // iCol번째 비트를 1로 설정.
+	}
+}
