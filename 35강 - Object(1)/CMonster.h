3fade20b890d6ac52fc8814e26503efca5190ae6
승환 지 <horong0602.dev@gmail.com>
2025-01-27 01:20:17 +0900

    오브젝트 복제

---
+++ 35강 - Object(1)/CMonster.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "CObject.h"
+
+class CMonster : public CObject
+{
+private:
+	Vec2  m_vCenterPos;	  // 이동 반경의 중심 좌표.
+	float m_fSpeed;		  // 이동 속도.
+	float m_fMaxDistance; // 최대 이동 반경.
+	int   m_iDir;		  // 이동 방향.
+	int	  m_iHP;          // 몬스터 체력.
+
+public:
+	float GetSpeed() { return m_fSpeed; }
+	void SetSpeed(float _f) { m_fSpeed = _f; }
+	void SetMoveDistance(float _f) { m_fMaxDistance = _f; }
+	void SetCenterPos(Vec2 _vPos) { m_vCenterPos = _vPos; }
+
+public:
+	virtual void OnCollisionEnter(CCollider* _pOther);
+
+public:
+	virtual void update();
+
+	CLONE(CMonster);
+
+public:
+	CMonster();
+	~CMonster();
+};
---
+++ 35강 - Object(1)/CObject.cpp
@@ -0,0 +1,59 @@
+#include "pch.h"
+#include "CObject.h"
+#include "CCollider.h"
+
+CObject::CObject()
+	: m_vPos{}
+	, m_vScale{}
+	, m_pCollider(nullptr)
+	, m_bAlive(true)
+{
+}
+
+CObject::CObject(const CObject& _origin)
+	: m_strName(_origin.m_strName)
+	, m_vPos(_origin.m_vPos)
+	, m_vScale(_origin.m_vScale)
+	, m_pCollider(nullptr)
+	, m_bAlive(true)
+{
+	// 원본 오브젝트의 충돌 영역 복사.
+	m_pCollider = new CCollider(*_origin.m_pCollider);
+	m_pCollider->m_pOwner = this;
+}
+
+CObject::~CObject()
+{
+	if (nullptr != m_pCollider)
+		delete m_pCollider;
+}
+
+void CObject::finalupdate()
+{
+	if (m_pCollider)
+		m_pCollider->finalupdate();
+}
+
+void CObject::render(HDC _dc)
+{
+	Rectangle(_dc, int(m_vPos.x - m_vScale.x / 2.f)
+		, int(m_vPos.y - m_vScale.y / 2.f)
+		, int(m_vPos.x + m_vScale.x / 2.f)
+		, int(m_vPos.y + m_vScale.y / 2.f));
+
+	component_render(_dc);
+}
+
+void CObject::component_render(HDC _dc)
+{
+	if (nullptr != m_pCollider)
+	{
+		m_pCollider->render(_dc);
+	}
+}
+
+void CObject::CreateCollider()
+{
+	m_pCollider = new CCollider;
+	m_pCollider->m_pOwner = this;
+}
