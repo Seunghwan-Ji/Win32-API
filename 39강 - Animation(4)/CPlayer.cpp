aaf2a9ba3d195f23615651642d13dcf20f27ffa2
승환 지 <horong0602.dev@gmail.com>
2025-02-02 00:52:19 +0900

    TOP VIEW 2D 게임에서의 오브젝트와 텍스쳐간 상대적 위치

---
+++ 39강 - Animation(4)/CPlayer.cpp
@@ -0,0 +1,98 @@
+#include "pch.h"
+#include "CPlayer.h"
+#include "CScene.h"
+#include "CSceneMgr.h"
+#include "CKeyMgr.h"
+#include "CTimeMgr.h"
+#include "CMissile.h"
+#include "CPathMgr.h"
+#include "CResMgr.h"
+#include "CTexture.h"
+#include "CCollider.h"
+#include "CAnimator.h"
+#include "CAnimation.h"
+
+CPlayer::CPlayer()
+{
+	// Texture 로딩하기.
+	// m_pTex = CResMgr::GetInst()->LoadTexture(L"PlayerTex", L"texture\\Player.bmp");
+
+	CreateCollider(); // 충돌 영역 생성.
+	GetCollider()->SetOffsetPos(Vec2(0.f, 12.f)); // 충돌 영역 위치 조정.
+	GetCollider()->SetScale(Vec2(20.f, 40.f)); // 충돌 영역 사이즈 조정.
+
+	// Texture 로딩하기.
+	CTexture* pTex = CResMgr::GetInst()->LoadTexture(L"PlayerTex", L"texture\\link_0.bmp");
+
+	CreateAnimator();
+	GetAnimator()->CreateAnimation(L"WALK_DOWN", pTex, Vec2(0.f, 260.f), Vec2(60.f, 65.f)
+		, Vec2(60.f, 0.f), 0.08f, 10);
+	GetAnimator()->Play(L"WALK_DOWN", true);
+
+	// 텍스쳐를 오브젝트 위치보다 위쪽에 배치.
+	CAnimation* pAnim = GetAnimator()->FindAnimation(L"WALK_DOWN");
+	for (int i = 0; i < pAnim->GetMaxFrame(); ++i)
+	{
+		pAnim->GetFrame(i).vOffset = Vec2(0.f, -20.f);
+	}
+}
+
+CPlayer::~CPlayer()
+{
+
+}
+
+void CPlayer::update()
+{
+	Vec2 vPos = GetPos(); // 부모 클래스를 통해 좌표 받아오기.
+
+	if (KEY_HOLD(KEY::UP))
+	{
+		vPos.y -= 200.f * fDT;
+	}
+
+	if (KEY_HOLD(KEY::DOWN))
+	{
+		vPos.y += 200.f * fDT;
+	}
+
+	if (KEY_HOLD(KEY::LEFT))
+	{
+		vPos.x -= 200.f * fDT;
+	}
+
+	if (KEY_HOLD(KEY::RIGHT))
+	{
+		vPos.x += 200.f * fDT;
+	}
+
+	if (KEY_TAP(KEY::SPACE))
+	{
+		CreateMissile();
+	}
+
+	SetPos(vPos); // 부모 클래스를 통해 좌표 갱신.
+
+	GetAnimator()->update();
+}
+
+void CPlayer::render(HDC _dc)
+{
+	// 컴포넌트(충돌체, etc...)가 있는 경우 렌더.
+	component_render(_dc);
+}
+
+void CPlayer::CreateMissile()
+{
+	Vec2 vMissilePos = GetPos();
+	vMissilePos.y -= GetScale().y / 2.f;
+
+	// Missile Object
+	CMissile* pMissile = new CMissile;
+	pMissile->SetName(L"Missile_Player");
+	pMissile->SetPos(vMissilePos);
+	pMissile->SetScale(Vec2(25.f, 25.f));
+	pMissile->SetDir(Vec2(0.f, -1.f));
+
+	CreateObject(pMissile, GROUP_TYPE::PROJ_PLAYER);
+}
---
+++ 39강 - Animation(4)/struct.h
@@ -0,0 +1,89 @@
+#pragma once
+
+struct Vec2 // 크기와 방향을 의미하는 벡터.
+{
+	float x;
+	float y;
+
+public:
+	float Length() // 벡터의 길이.(빗변의 길이)
+	{
+		return sqrt(x * x + y * y); // sqrt: 루트 함수.
+	}
+
+	Vec2& Normalize() // 벡터를 정규화(단위 벡터로 변환)하는 함수.
+	{
+		float fLen = Length();
+
+		assert(fLen != 0.f); // 길이가 0 이면 예외처리.
+
+		x /= fLen;
+		y /= fLen;
+
+		return *this;
+	}
+
+public:
+	Vec2& operator = (POINT _pt)
+	{
+		x = (float)_pt.x;
+		y = (float)_pt.y;
+	}
+
+	Vec2 operator + (Vec2 _vOther)
+	{
+		return Vec2(x + _vOther.x, y + _vOther.y);
+	}
+
+	void operator += (Vec2 _vOther)
+	{
+		x += _vOther.x;
+		y += _vOther.y;
+	}
+
+	Vec2 operator - (Vec2 _vOther)
+	{
+		return Vec2(x - _vOther.x, y - _vOther.y);
+	}
+
+	Vec2 operator * (Vec2 _vOther)
+	{
+		return Vec2(x * _vOther.x, y * _vOther.y);
+	}
+
+	Vec2 operator * (int _i)
+	{
+		return Vec2(x * (float)_i, y * (float)_i);
+	}
+
+	Vec2 operator / (Vec2 _vOther)
+	{
+		assert(!(0.f == _vOther.x || 0.f == _vOther.y));
+		return Vec2(x / _vOther.x, y / _vOther.y);
+	}
+
+public:
+	Vec2()
+		: x(0.f)
+		, y(0.f)
+	{
+	}
+
+	Vec2(float _x, float _y)
+		: x(_x)
+		, y(_y)
+	{
+	}
+
+	Vec2(int _x, int _y)
+		: x((float)_x)
+		, y((float)_y)
+	{
+	}
+
+	Vec2(const POINT& _pt)
+		: x((float)_pt.x)
+		, y((float)_pt.y)
+	{
+	}
+};
