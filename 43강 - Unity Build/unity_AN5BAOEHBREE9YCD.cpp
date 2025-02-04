afb6ca03c08ff8168b0447af9232e9131e3be4a9
승환 지 <horong0602.dev@gmail.com>
2025-02-05 02:33:44 +0900

    컴파일 속도 향상법

---
+++ 43강 - Unity Build/unity_AN5BAOEHBREE9YCD.cpp
@@ -0,0 +1,36 @@
+
+#include "C:\Users\Seunghwan\Documents\Coding\C++\Win32-API\TIL\CResMgr.cpp"
+
+
+#include "C:\Users\Seunghwan\Documents\Coding\C++\Win32-API\TIL\CScene.cpp"
+
+
+#include "C:\Users\Seunghwan\Documents\Coding\C++\Win32-API\TIL\CSceneMgr.cpp"
+
+
+#include "C:\Users\Seunghwan\Documents\Coding\C++\Win32-API\TIL\CScene_Start.cpp"
+
+
+#include "C:\Users\Seunghwan\Documents\Coding\C++\Win32-API\TIL\CScene_Tool.cpp"
+
+
+#include "C:\Users\Seunghwan\Documents\Coding\C++\Win32-API\TIL\CSound.cpp"
+
+
+#include "C:\Users\Seunghwan\Documents\Coding\C++\Win32-API\TIL\CTexture.cpp"
+
+
+#include "C:\Users\Seunghwan\Documents\Coding\C++\Win32-API\TIL\CTimeMgr.cpp"
+
+
+#include "C:\Users\Seunghwan\Documents\Coding\C++\Win32-API\TIL\func.cpp"
+
+
+#include "C:\Users\Seunghwan\Documents\Coding\C++\Win32-API\TIL\SelectGDI.cpp"
+
+
+#include "C:\Users\Seunghwan\Documents\Coding\C++\Win32-API\TIL\TIL.cpp"
+
+
+#include "C:\Users\Seunghwan\Documents\Coding\C++\Win32-API\TIL\CAnimator.cpp"
+
---
+++ 43강 - Unity Build/unity_SD59YXROTO220BFP.cpp
@@ -0,0 +1,39 @@
+
+#include "C:\Users\Seunghwan\Documents\Coding\C++\Win32-API\TIL\CAnimation.cpp"
+
+
+#include "C:\Users\Seunghwan\Documents\Coding\C++\Win32-API\TIL\CCamera.cpp"
+
+
+#include "C:\Users\Seunghwan\Documents\Coding\C++\Win32-API\TIL\CCollider.cpp"
+
+
+#include "C:\Users\Seunghwan\Documents\Coding\C++\Win32-API\TIL\CCollisionMgr.cpp"
+
+
+#include "C:\Users\Seunghwan\Documents\Coding\C++\Win32-API\TIL\CCore.cpp"
+
+
+#include "C:\Users\Seunghwan\Documents\Coding\C++\Win32-API\TIL\CEventMgr.cpp"
+
+
+#include "C:\Users\Seunghwan\Documents\Coding\C++\Win32-API\TIL\CKeyMgr.cpp"
+
+
+#include "C:\Users\Seunghwan\Documents\Coding\C++\Win32-API\TIL\CMissile.cpp"
+
+
+#include "C:\Users\Seunghwan\Documents\Coding\C++\Win32-API\TIL\CMonster.cpp"
+
+
+#include "C:\Users\Seunghwan\Documents\Coding\C++\Win32-API\TIL\CObject.cpp"
+
+
+#include "C:\Users\Seunghwan\Documents\Coding\C++\Win32-API\TIL\CPathMgr.cpp"
+
+
+#include "C:\Users\Seunghwan\Documents\Coding\C++\Win32-API\TIL\CPlayer.cpp"
+
+
+#include "C:\Users\Seunghwan\Documents\Coding\C++\Win32-API\TIL\CRes.cpp"
+
