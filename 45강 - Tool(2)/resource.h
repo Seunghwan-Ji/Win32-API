acc84f301ca631cf13e0004abc86d9d0b032c85e
승환 지 <horong0602.dev@gmail.com>
2025-02-07 03:57:16 +0900

    타일 개수를 지정할 수 있는 UI

---
+++ 45강 - Tool(2)/resource.h
@@ -0,0 +1,38 @@
+﻿// 리소스 뷰에서 편집한 속성 ID 값이 이 헤더 파일에 추가된다.
+// 그렇기 때문에 리소스 뷰 편집 중에는 이 파일을 열 수 없다.
+// 반대로 이 파일을 수정 중에는 리소스 뷰를 열 수 없다.
+// 서로 영향을 주기 때문에 동시에 같이 작업할 수 없게 방어 하도록 설계되었기 때문이다.
+
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// TIL.rc에서 사용되고 있습니다.
+//
+#define IDC_MYICON                      2
+#define IDD_TIL_DIALOG                  102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_TIL                         107
+#define IDI_SMALL                       108
+#define IDC_TIL                         109
+#define IDR_MAINFRAME                   128
+#define IDD_TILE_COUNT                  129
+#define IDC_EDIT1                       1000
+#define IDC_EDIT2                       1001
+#define ID_32771                        32771
+#define ID_MENU                         32772
+#define ID_MENU_TILE_COUNT              32773
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32775
+#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
